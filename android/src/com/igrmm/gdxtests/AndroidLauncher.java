f46657da1aec5ebd54880bcc0ac9c69f3dc73d70
igrmm <igrmmdev@gmail.com>
2022-12-13 18:42:42 -0400

    add project made with gdx wizard

---
+++ android/src/com/igrmm/gdxtests/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.igrmm.gdxtests;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.igrmm.gdxtests.GdxTests;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxTests(), config);
+	}
+}
---
+++ desktop/src/com/igrmm/gdxtests/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.igrmm.gdxtests;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.igrmm.gdxtests.GdxTests;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("gdx-tests");
+		new Lwjgl3Application(new GdxTests(), config);
+	}
+}
